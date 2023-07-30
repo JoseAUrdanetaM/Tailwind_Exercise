aa3487f0b0a615cf1e4a6aac03183791343c59bd
Jose <urdaneta.j@outlook.com>
2023-07-30 17:09:08 -0500

    Upd-3

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const hamburguerMenu = document.querySelector("#hamburguer");
 const menu = document.querySelector("#menu");
+const hLinks = document.querySelectorAll("#hLink");
+const body = document.querySelector("body");
+const moon = document.querySelector("#moon");
 
 hamburguerMenu.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   hamburguerMenu.classList.toggle("bg-white");
+});
+
+hLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    hamburguerMenu.classList.toggle("bg-white");
+  });
+});
+
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
 });
